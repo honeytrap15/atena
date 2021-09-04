0d75dea9f4055893233845b66a5b99f802cb19c3
takegami <tkgm15h@gmail.com>
2021-09-04 20:49:20 +0900

    郵便番号検索APIで住所を検索

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect } from 'react';
+import { useCallback, useState, } from 'react';
 import './App.css';
 import {
   FormControl,
@@ -17,12 +17,26 @@ import axios from 'axios';
 
 function App() {
 
+  const [target, setTarget] = useState('');
   const [address, setAddress] = useState('');
   const [zipcode, setZipcode] = useState('');
+  const [hideAddr, setHideAddr] = useState(false);
 
-  const onClickSearchAddress = useCallback(() => {
-      console.log("foo");
-  }, []);
+  const onClickSearchAddress = useCallback(async () => {
+    setHideAddr(true);
+    const r = await axios.get('/api/search', {
+      params: {
+        zipcode: zipcode
+      }
+    });
+    console.log(r.data);
+    if (!r.data.results) {
+      alert('該当する住所はありません');
+      return;
+    }
+    setAddress(r.data.results[0].address1 + r.data.results[0].address2 + r.data.results[0].address3);
+    setHideAddr(false);
+  }, [zipcode, setAddress]);
 
   const validateZipcode = useCallback(():boolean => {
     const pattern = /^[0-9]{3}-[0-9]{4}$/;
@@ -36,8 +50,8 @@ function App() {
               <Input
                   type="text"
                   placeholder="○○株式会社 ○○様"
-                  value={address}
-                  onChange={(e) => setAddress(e.target.value)} />
+                  value={target}
+                  onChange={(e) => setTarget(e.target.value)} />
           </FormControl>
           <FormControl id="zipcode" isRequired isInvalid={zipcode.length > 0 && !validateZipcode()}>
               <FormLabel mt={5}>郵便番号</FormLabel>
@@ -51,11 +65,19 @@ function App() {
           <FormControl isRequired>
               <FormLabel mt={5}>住所</FormLabel>
               <Flex>
-                  <Input type="text" placeholder="東京都新宿区西新宿２丁目８−１" />
+                  <Input
+                      className={hideAddr ? '' : 'hide'}
+                      type="text"
+                      placeholder="東京都新宿区西新宿２丁目８−１"
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
+                      />
                   <Button
                       ml={5}
                       leftIcon={<SearchIcon />}
-                      onClick={onClickSearchAddress}>郵便番号から検索</Button>
+                      onClick={onClickSearchAddress}
+                      isDisabled={!validateZipcode()}
+                      >郵便番号から検索</Button>
               </Flex>
           </FormControl>
           <FormControl>
